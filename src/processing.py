c3f5c0bf5677f5c6a9c70f04a95e20fb03873e55
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-24 01:04:34 +0300

    В модуле processing создали функцию sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,7 @@ def filter_by_state(transactions: list[dict[str, Any]], state: str = "EXECUTED")
     return filtered_transactions
 
 
+def sort_by_date(transactions: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """Функция сортировки операций по дате"""
+    sorted_transactions = sorted(transactions, key=lambda transaction: transaction["date"], reverse=True)
+    return sorted_transactions
