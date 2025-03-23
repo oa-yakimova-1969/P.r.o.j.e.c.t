f02f1d195ec73a22451e16fa16cb2229bb7f5877
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-24 02:47:52 +0300

    Финальные изменения и доработки

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,46 +1,47 @@
+from typing import Any
+
 import pytest
 
-from src.widget import get_date, mask_account_card
-
-
-@pytest.mark.parametrize(
-    "input_string, expected_string",
-    [
-        ("Maestro 1596837868705199", "Maestro 1596 83** **** 5199"),
-        ("Счет 64686473678894779589", "Счет **9589"),
-        ("Master Card 7158300734726758", "Master Card 7158 30** **** 6758"),
-        ("Счет 35383033474447895560", "Счет **5560"),
-        ("Visa Classic 6831982476737658", "Visa Classic 6831 98** **** 7658"),
-        ("Visa Platinum 8990922113665229", "Visa Platinum 8990 92** **** 5229"),
-        ("Visa Gold 5999414228426353", "Visa Gold 5999 41** **** 6353"),
-        ("Счет 73654108430135874305", "Счет **4305"),
-    ],
-)
-def test_mask_account_card(input_string: str, expected_string: str) -> None:
-    assert mask_account_card(input_string) == expected_string
-
-
-def test_mask_account_card_incorrect() -> None:
-    assert mask_account_card("9999888877776666") == "Ошибка: некорректный ввод."
-    assert mask_account_card("Счет") == "Ошибка: некорректный ввод."
-
-
-def test_mask_account_card_empty() -> None:
-    assert mask_account_card("") == "Ошибка: некорректный ввод."
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.fixture
-def date() -> str:
-    return "2024-03-11T02:26:18.671407"
+def transactions() -> list[dict[str, Any]]:
+    return [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
-def test_get_date(date: str) -> None:
-    assert get_date(date) == "11.03.2024"
+def test_filter_by_state(transactions: list[dict[str, Any]], state: str = "EXECUTED") -> None:
+    assert filter_by_state(transactions, state="EXECUTED") == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
 
 
-def test_get_date_incorrect() -> None:
-    assert get_date("T03:36:20.507342") == "Введены некорректные данные"
+def test_filter_by_state_canceled(transactions: list[dict[str, Any]], state: str = "CANCELED") -> None:
+    assert filter_by_state(transactions, state="CANCELED") == [
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+    ]
 
 
-def test_get_date_empty() -> None:
-    assert get_date("") == "Введены некорректные данные"
+def test_sort_by_date(transactions: list[dict[str, Any]], reverse: bool = True) -> None:
+    assert sort_by_date(transactions, reverse=True) == [
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+    ]
+
+
+def test_sort_by_date_false(transactions: list[dict[str, Any]], reverse: bool = False) -> None:
+    assert sort_by_date(transactions, reverse=False) == [
+        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+    ]
